34bdd114ad55840331a3d7e1ff5a8faa1c1bef33
Mariano-gago <mfgago05@gmail.com>
2022-10-16 16:41:07 -0300

    Agregar archivos de proyecto.

---
+++ Web Core 6 ABM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Core_6_ABM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
